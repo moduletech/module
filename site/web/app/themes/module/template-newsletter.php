59b3333a6b1595a188f1d90d2036a0e2a271a958
Chip Kennedy <chippykennedy32@gmail.com>
2018-09-05 22:44:35 -0400

    Add Newsletter Page Template

---
+++ site/web/app/themes/module/template-newsletter.php
@@ -0,0 +1,14 @@
+<?php /*
+/* Template Name: Newsletter
+*/
+get_header();?>
+
+<main class="main" itemscope itemType="https://schema.org/WebPage">
+    <section class="contact-form u-center-block">
+        <header class="contact-form__header">
+            <h2 class="contact-form__header-title u-text-center">
+            </h2>
+        </header>
+				<?php include('templates/_mailing-list.php'); ?>
+    </section>
+</div>
