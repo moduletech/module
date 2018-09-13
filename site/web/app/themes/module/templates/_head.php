a6c59e0991a9698e693e8631240841ff2688d8c3
Chip Kennedy <chippykennedy32@gmail.com>
2018-09-13 01:04:10 -0400

    Add Google Tag Mangager Code to Head

--- site/web/app/themes/module/templates/_head.php
+++ site/web/app/themes/module/templates/_head.php
@@ -28,5 +28,17 @@
       </script>
     <?php } ?> 
 
+		<!-- Google Tag Manager -->
+    <?php if (!is_user_logged_in()) {?>
+			<script>
+				(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+				new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+				j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+				'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+				})(window,document,'script','dataLayer','GTM-M5F4RXD');
+			</script>
+    <?php } ?> 
+		<!-- End Google Tag Manager -->
+
     <!-- End Facebook Pixel Code -->
 </head>
