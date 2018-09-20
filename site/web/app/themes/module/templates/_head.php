4ba4f6995bb7e253facab6c328fdc990a6f2ced9
Chip Kennedy <chippykennedy32@gmail.com>
2018-09-20 14:31:40 -0400

    Clean Up GA/GTM Header Scripts

--- site/web/app/themes/module/templates/_head.php
+++ site/web/app/themes/module/templates/_head.php
@@ -14,20 +14,6 @@
     <script>try{Typekit.load({ async: true });}catch(e){}</script>
     <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.min.css"/>
 
-    <!-- Google Analytics-->
-    <?php if (!is_user_logged_in()) {?>
-      <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-93265332-1', 'auto');
-        ga('send', 'pageview');
-
-      </script>
-    <?php } ?> 
-
 		<!-- Google Tag Manager -->
     <?php if (!is_user_logged_in()) {?>
 			<script>
