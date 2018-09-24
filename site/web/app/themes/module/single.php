6b43522ed8e9572070e5dee05b4e5f8a86249f99
Toby Benjamin <toby@tobybenjamin.com>
2018-09-24 11:36:47 -0400

    Blog single
    Signed-off-by: Toby Benjamin <toby@tobybenjamin.com>

--- site/web/app/themes/module/single.php
+++ site/web/app/themes/module/single.php
@@ -11,7 +11,12 @@ get_header(); ?>
 
 <main class="blog-main">
     <div class="u-container">
-        <section class="blog-loop">
+        <section class="blog-single">
+          <div class="double-header">
+            <h2 class="title"><?= get_the_title() ?></h2>
+            <!--<h4 class="subtitle">Subtext</h4>-->
+          </div>
+
             <?php if (have_posts()) : ?>
                 <?php while (have_posts()) :
                     the_post(); ?>
@@ -25,49 +30,19 @@ get_header(); ?>
                             <meta itemprop="height" content="400">
                         </div>
                         <div class="blog-loop__post-content">
-                            <h2 class="blog-loop__post-title" itemprop="headline">
-                                <a class="u-link-black" href="<?php the_permalink(); ?>">
-                                    <?php the_title(); ?>
-                                </a>
-                            </h2>
-                            <div class="blog-loop__post-meta">
-                                <p>
-                                    <span itemprop="author" itemscope itemtype="https://schema.org/Person">
-                                        Posted by <span itemprop="name">Module</span>
-                                    </span> / 
-                                    <time datetime="<?php the_time('Y-m-d'); ?>" pubdate itemprop="datePublished">
-                                    <meta itemprop="dateModified" content="<?php the_time('Y-m-d'); ?>"/>
-                                    Published on <?php echo get_the_date('F j, Y'); ?></time>
-                                    <?php
-                                    if (is_user_logged_in()) {
-                                        echo " / ";
-                                        edit_post_link('Edit Post');
-                                    }
-                                    ?>
-                                </p>
-                            </div><!-- /blog-loop__post-meta-->
-                            <div itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
-                                <div itemprop="logo" itemscope itemtype="https://schema.org/ImageObject">
-                                    <meta itemprop="url" content="http://mach1.com/wp-content/uploads/2017/07/schema_logo.png">
-                                    <meta itemprop="width" content="620">
-                                    <meta itemprop="height" content="73">
-                                </div>
-                                <meta itemprop="name" content="MACH1">
-                            </div>          
                             <div class="blog-content" itemprop="articleBody">
                                 <?php the_content();?>
                             </div>
                             <div class="blog-social">
                                 <hr>
                                 <?php echo do_shortcode('[share]');?>
-                            </div>  
+                            </div>
                         </div><!-- /blog-loop__post-content-->
                     </article>
                 <?php endwhile; ?>
                 <?php the_posts_navigation(); ?>
             <?php endif; ?>
         </section>
-        <?php get_sidebar(); ?>
     </div>
 </main>
 <?php get_footer(); ?>
